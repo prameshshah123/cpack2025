135f35ba0547324e526c4507f157e324ad55ce2a
prameshshah123 <cpackprint@gmail.com>
2025-12-13 13:10:42 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
-// pages/_app.js
+// pages/_app.js - SIMPLIFIED VERSION
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        * {
+          font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, sans-serif;
+        }
+      `}</style>
+    </>
+  );
 }
 
 export default MyApp;
